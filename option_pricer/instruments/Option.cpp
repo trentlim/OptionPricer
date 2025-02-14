8fa3db3b1ae1a53c9e0dfaa0d45ffa4b815cebfc
Trent Lim <trent_lim_nz@yahoo.com>
2025-02-14 13:24:21 +0100

    Move Option implementation into source file

---
+++ option_pricer/instruments/Option.cpp
@@ -0,0 +1,40 @@
+#include "Option.h"
+#include <stdexcept>
+
+Option::Option(Type type, double S, double K, double r, double sigma, double T)
+    : type_ { type }
+    , S_ { S }
+    , K_ { K }
+    , r_ { r }
+    , sigma_ { sigma }
+    , T_ { T }
+{
+    if (S <= 0.0)
+        throw std::invalid_argument("Underlying price S must be greater than 0.");
+    if (K <= 0.0)
+        throw std::invalid_argument("Strike price K must be greater than 0.");
+    if (sigma <= 0.0)
+        throw std::invalid_argument("Volatility sigma must be greater than 0.");
+    if (T <= 0.0)
+        throw std::invalid_argument("Time to maturity T must be greater than 0.");
+}
+
+void Option::perform_calculations()
+{
+    double payoff_sum { 0 };
+    const double number_of_simulations { 100000 };
+
+    const double drift { (r_ - 0.5 * sigma_ * sigma_) * T_ };
+    const double diffusion { sigma_ * sqrt(T_) };
+
+    for (int i { 0 }; i < number_of_simulations; ++i) {
+        double gaussian { Random::get_gaussian(0.0, 1.0) };
+
+        double ST { S_ * exp(drift + diffusion * gaussian) };
+        double payoff { type_ ? std::max(ST - K_, 0.0) : std::max(K_ - ST, 0.0) };
+        payoff_sum += payoff;
+    }
+
+    double average_payoff { payoff_sum / number_of_simulations };
+    price_ = exp(-r_ * T_) * average_payoff;
+}
--- option_pricer/instruments/Option.h
+++ option_pricer/instruments/Option.h
@@ -12,15 +12,7 @@ public:
         call,
     };
 
-    Option(Type type, double S, double K, double r, double sigma, double T)
-        : type_ { type }
-        , S_ { S }
-        , K_ { K }
-        , r_ { r }
-        , sigma_ { sigma }
-        , T_ { T }
-    {
-    }
+    Option(Type type, double S, double K, double r, double sigma, double T);
 
     double S() const { return S_; }
     double K() const { return K_; }
@@ -29,25 +21,7 @@ public:
     double T() const { return T_; }
 
 protected:
-    void perform_calculations() override
-    {
-        double payoff_sum { 0 };
-        const double number_of_simulations { 100000 };
-
-        const double drift { (r_ - 0.5 * sigma_ * sigma_) * T_ };
-        const double diffusion { sigma_ * sqrt(T_) };
-
-        for (int i { 0 }; i < number_of_simulations; ++i) {
-            double gaussian { Random::get_gaussian(0.0, 1.0) };
-
-            double ST { S_ * exp(drift + diffusion * gaussian) };
-            double payoff { type_ ? std::max(ST - K_, 0.0) : std::max(K_ - ST, 0.0) };
-            payoff_sum += payoff;
-        }
-
-        double average_payoff { payoff_sum / number_of_simulations };
-        price_ = exp(-r_ * T_) * average_payoff;
-    }
+    void perform_calculations();
 
 private:
     Type type_ {}; // Call or Put
